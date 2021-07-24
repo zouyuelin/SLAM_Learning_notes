7846e2bc89d2ed0e17932227f0c8dee0cc2700bf
yueyun <58660028+zouyuelin@users.noreply.github.com>
2021-07-24 23:18:27 +0800

    Add files via upload

---
+++ StereoCalibration/calibrate.cpp
@@ -0,0 +1,313 @@
+﻿/*
+    Pl = Rlw * Pw+Tlw;
+    Pr = Rrw * Pw+Trw;
+
+    --Pr = Rrl * Pl + Trl
+   so =>
+    Rrl = Rrw * Rlw^T
+    Trl = Trw - Rrl * Tlw
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/calib3d.hpp"
+#include "opencv2/imgproc.hpp"
+
+using namespace std;
+using namespace cv;
+
+void calibCamera(vector<string> &files, cv::Size boardSize, float squareSize,
+                string imagePath,
+                cv::Mat &cameraMatrix,
+                cv::Mat &distCoeffs,
+                std::vector<std::vector<cv::Point2f>> &imagePoints,
+                cv::Size &imageSize,
+                std::vector<std::vector<cv::Point3f>> &objectPoints,
+                std::vector<cv::Mat> &rvecs, std::vector<cv::Mat> &tvecs
+                 );
+
+int main(int argc,char**argv)
+{
+    float squareSize = 1.0;
+    if(argc<5)
+    {
+        cout<<"./calibrationCamera [leftImagePath] [rightImagePath] [width-1] [height-1]\n";
+        cout<<"example: 如果你是10*6（宽10，高6）的棋盘，那么输入的就是 9 5\n";
+        return -1;
+    }
+
+    if(argc == 6)
+    {
+        squareSize = atof(argv[5]);
+    }
+
+    cv::Size boardSize(atoi(argv[3]),atoi(argv[4]));
+    cv::Size imageSize; //empty, get the data when running the calibCamera
+
+
+    vector<string> leftfiles,rightfiles;
+    string leftImagePath(argv[1]);
+    string rightImagePath(argv[2]);
+    glob(leftImagePath,leftfiles);
+    glob(rightImagePath,rightfiles);
+
+
+    //saver
+    std::vector<std::vector<cv::Point3f>> objectPoints;
+    std::vector<std::vector<cv::Point2f>> imagePointsleft;
+    std::vector<std::vector<cv::Point2f>> imagePointsRight;
+
+
+    //-----------------------------calibrate--------------------------
+    //left camera matrix
+    cv::Mat cameraMatrixLeft;  //Camera intrinsic Matrix
+    cv::Mat distCoeffsLeft;    //five parameters of distCoeffs，(k1,k2,p1,p2[,k3[,k4,k5,k6]])
+    std::vector<cv::Mat> rvecsLeft, tvecsLeft;
+    cout<<endl<<"-------------------Left Camera intrinsic Matrix--------------------\n";
+    calibCamera(leftfiles,boardSize,squareSize,leftImagePath,cameraMatrixLeft,distCoeffsLeft,imagePointsleft,imageSize,objectPoints,rvecsLeft,tvecsLeft);
+    objectPoints.clear();
+
+    //right camera matrix
+    cv::Mat cameraMatrixRight;  //Camera intrinsic Matrix
+    cv::Mat distCoeffsRight;    //five parameters of distCoeffs，(k1,k2,p1,p2[,k3[,k4,k5,k6]])
+    std::vector<cv::Mat> rvecsRight, tvecsRight;
+    cout<<endl<<"-------------------Right Camera intrinsic Matrix--------------------\n";
+    calibCamera(rightfiles,boardSize,squareSize,rightImagePath,cameraMatrixRight,distCoeffsRight,imagePointsRight,imageSize,objectPoints,rvecsRight,tvecsRight);
+
+    //
+    /*
+        Pl = Rlw * Pw+Tlw;
+        Pr = Rrw * Pw+Trw;
+
+        --Pr = Rrl * Pl + Trl
+       so =>
+        Rrl = Rrw * Rlw^T
+        Trl = Trw - Rrl * Tlw
+
+    */
+    //test the eqution
+    /*
+    Mat Rr,Rl;
+    cv::Rodrigues(rvecsRight[0],Rr);
+    cv::Rodrigues(rvecsLeft[0],Rl);
+    cout<<endl<<Rr*Rl.inv()<<endl;
+    */
+
+
+
+    //-----------------------------stereo calibrate-------------------------
+    Mat cameraMatrix[2];
+    cameraMatrix[0] = initCameraMatrix2D(objectPoints,imagePointsleft,imageSize,0);
+    cameraMatrix[1] = initCameraMatrix2D(objectPoints,imagePointsRight,imageSize,0);
+
+    //you can also this parameters if you do not calibrate the single camera;
+    /*Then,use the codes to substitute old codes
+     *
+     *  F = findFundamentalMat(Mat(allimgpt[0]), Mat(allimgpt[1]), FM_8POINT, 0, 0);
+        Mat H1, H2;
+        stereoRectifyUncalibrated(Mat(allimgpt[0]), Mat(allimgpt[1]), F, imageSize, H1, H2, 3);
+
+        R1 = cameraMatrix[0].inv()*H1*cameraMatrix[0];
+        R2 = cameraMatrix[1].inv()*H2*cameraMatrix[1];
+        P1 = cameraMatrix[0];
+        P2 = cameraMatrix[1];
+    */
+    cout<<"initCameraMatrix2D left:\n"<<cameraMatrix[0]<<endl;
+    cout<<"initCameraMatrix2D right:\n"<<cameraMatrix[1]<<endl;
+
+
+
+    cout<<endl<<"********************stereo calibrate running******************\n";
+    Mat R, T, E, F;
+
+    double rms = stereoCalibrate(objectPoints,
+                    imagePointsleft, imagePointsRight,
+                    cameraMatrixLeft, distCoeffsLeft,
+                    cameraMatrixRight, distCoeffsRight,
+                    imageSize, R, T, E, F,
+                    CALIB_FIX_INTRINSIC,// CALIB_USE_INTRINSIC_GUESS
+                    TermCriteria(TermCriteria::COUNT+TermCriteria::EPS, 100, 1e-5) );
+
+    cout << "done with RMS error=" << rms << endl;
+
+
+    Mat R1, R2, P1, P2, Q;
+    Rect validRoi[2];
+
+    //双目矫正
+    stereoRectify(cameraMatrixLeft, distCoeffsLeft,
+                  cameraMatrixRight, distCoeffsRight,
+                  imageSize, R, T, R1, R2, P1, P2, Q,
+                  CALIB_ZERO_DISPARITY, -1, imageSize, &validRoi[0], &validRoi[1]);
+
+    cout<<"R is :\n"<<R<<endl;
+    cout<<"T is :\n"<<T<<endl;
+    cout<<"R1 is :\n"<<R1<<endl;
+    cout<<"R2 is :\n"<<R2<<endl;
+    cout<<"P1 is :\n"<<P1<<endl;
+    cout<<"P1 is :\n"<<P2<<endl;
+    cout<<"Q is :\n"<<Q<<endl;
+
+    //remap
+    Mat rmap[2][2];
+    initUndistortRectifyMap(cameraMatrixLeft, distCoeffsLeft, R1, P1, imageSize, CV_16SC2, rmap[0][0], rmap[0][1]);
+    initUndistortRectifyMap(cameraMatrixRight, distCoeffsRight, R2, P2, imageSize, CV_16SC2, rmap[1][0], rmap[1][1]);
+
+    for(size_t i=0;i<leftfiles.size();i++)
+    {
+        Mat il = imread(leftfiles[i],0),ilremap;
+        Mat ir = imread(rightfiles[i],0),irremap;
+
+        if(il.empty() || ir.empty())
+            continue;
+
+        remap(il, ilremap, rmap[0][0], rmap[0][1], INTER_LINEAR);
+        remap(ir, irremap,  rmap[1][0], rmap[1][1], INTER_LINEAR);
+
+        Mat srcAnddst(ilremap.rows,ilremap.cols + irremap.cols,ilremap.type());
+        Mat submat =srcAnddst.colRange(0,ilremap.cols);
+           ilremap.copyTo(submat);
+           submat = srcAnddst.colRange(ilremap.cols,ilremap.cols + irremap.cols);
+           irremap.copyTo(submat);
+
+        imshow("il",srcAnddst);
+        //imshow("ir",irremap);
+        waitKey(300);
+    }
+
+    //save the config parameters
+    //clear the old .yaml file
+    String path = "./calib.yaml";
+    ofstream fs(path);
+    fs.clear();
+
+    fs << "# ------left camera Intrinsic--------"<<endl;
+    fs << "Left.Camera.fx:  " << cameraMatrixLeft.at<double>(0,0)<<endl;
+    fs << "Left.Camera.fy:  " << cameraMatrixLeft.at<double>(1,1)<<endl;
+    fs << "Left.Camera.cx:  " << cameraMatrixLeft.at<double>(0,2)<<endl;
+    fs << "Left.Camera.cy:  " << cameraMatrixLeft.at<double>(1,2)<<endl<<endl;
+
+    fs << "# ------left camera Distortion--------"<<endl;
+    fs << "Left.Camera.k1:  " << distCoeffsLeft.at<double>(0,0)<<endl;
+    fs << "Left.Camera.k2:  " << distCoeffsLeft.at<double>(0,1)<<endl;
+    fs << "Left.Camera.p1:  " << distCoeffsLeft.at<double>(0,2)<<endl;
+    fs << "Left.Camera.p2:  " << distCoeffsLeft.at<double>(0,3)<<endl;
+    fs << "Left.Camera.k3:  " << distCoeffsLeft.at<double>(0,4)<<endl<<endl;
+
+    fs <<endl<<endl;
+
+    fs << "# ------right camera Intrinsic--------"<<endl;
+    fs << "Right.Camera.fx:  " << cameraMatrixRight.at<double>(0,0)<<endl;
+    fs << "Right.Camera.fy:  " << cameraMatrixRight.at<double>(1,1)<<endl;
+    fs << "Right.Camera.cx:  " << cameraMatrixRight.at<double>(0,2)<<endl;
+    fs << "Right.Camera.cy:  " << cameraMatrixRight.at<double>(1,2)<<endl<<endl;
+
+    fs << "# ------right camera Distortion--------"<<endl;
+    fs << "Right.Camera.k1:  " << distCoeffsRight.at<double>(0,0)<<endl;
+    fs << "Right.Camera.k2:  " << distCoeffsRight.at<double>(0,1)<<endl;
+    fs << "Right.Camera.p1:  " << distCoeffsRight.at<double>(0,2)<<endl;
+    fs << "Right.Camera.p2:  " << distCoeffsRight.at<double>(0,3)<<endl;
+    fs << "Right.Camera.k3:  " << distCoeffsRight.at<double>(0,4)<<endl<<endl;
+
+    fs.close();
+
+    //R, T, R1, R2, P1, P2, Q,
+    cv::FileStorage f(path,cv::FileStorage::APPEND);
+    f.writeComment(" \n------camera parameters saved by yaml data--------",true);
+    f<<"R"<<R;
+    f<<"T"<<T;
+    f<<"R1"<<R1;
+    f<<"R2"<<R2;
+    f<<"P1"<<P1;
+    f<<"P2"<<P2;
+    f<<"Q"<<Q;
+
+    f.release();
+
+    return 0;
+}
+
+void calibCamera(vector<string> &files, cv::Size boardSize,
+                float squareSize,
+                string imagePath, 
+                cv::Mat &cameraMatrix,
+                cv::Mat &distCoeffs,
+                std::vector<std::vector<cv::Point2f>> &imagePoints,
+                cv::Size &imageSize,
+                std::vector<std::vector<cv::Point3f>> &objectPoints,
+                std::vector<cv::Mat> &rvecs, std::vector<cv::Mat> &tvecs)
+{
+
+        //data objectCorners
+    vector<Point3f> objectCorners;
+
+    vector<Point2f> imageCorners;
+
+        //get the Corners' position
+    for (int i = 0; i < boardSize.height; i++)
+    {
+       for (int j = 0; j < boardSize.width; j++)
+       {
+          objectCorners.push_back(cv::Point3f(squareSize*i, squareSize*j, 0.0f));
+       }
+       
+    }
+
+    for(size_t i =0;i<files.size();i++)
+    {
+        Mat image = imread(files[i]);
+        Mat gray;
+        if(image.empty())
+            continue;
+
+        cvtColor(image,gray,COLOR_BGR2GRAY);
+        //get Chessboard Corners
+        bool found = cv::findChessboardCorners(gray, boardSize,imageCorners);
+
+        if(found)
+        {
+            TermCriteria  termCriteria;
+            termCriteria = TermCriteria(TermCriteria::MAX_ITER +TermCriteria::EPS,
+                                                             30,    //maxCount
+                                                             0.1);  //epsilon
+            cornerSubPix(gray,
+                         imageCorners,
+                         cv::Size(5, 5), // winSize
+                         cv::Size(-1, -1),
+                         termCriteria); // epsilon
+
+            imagePoints.push_back(imageCorners);
+            objectPoints.push_back(objectCorners);
+
+
+            cv::drawChessboardCorners(image, boardSize, imageCorners, found);
+            imshow("Corners on Chessboard", image);
+            waitKey(33);
+
+        }
+        imageSize = image.size();
+    }
+
+    cv::destroyAllWindows();
+
+    cout<<imageSize<<endl;
+
+    double rms = calibrateCamera(objectPoints, // 三维点
+                    imagePoints, // 图像点
+                    imageSize, // 图像尺寸
+                    cameraMatrix, // 输出相机矩阵
+                    distCoeffs, // 输出畸变矩阵
+                    rvecs, tvecs // Rs、Ts（外参）
+                    );
+
+    cout << " RMS error=" << rms << endl;
+
+
+    cout<<"K = \n"<<cameraMatrix<<endl;
+    cout<<"distCoeffs = \n"<<distCoeffs<<endl;
+}
---
+++ StereoCalibration/openCamera.cpp
@@ -0,0 +1,48 @@
+﻿#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc,char ** argv)
+{
+    if(argc < 3)
+    {
+        cout<<"please use : openCamera [leftImagePath] [rightImagePath]\n";
+        return -1;
+    }
+    VideoCapture capture(0);
+    capture.set(cv::CAP_PROP_FRAME_WIDTH,1280);
+    capture.set(cv::CAP_PROP_FRAME_HEIGHT,480);
+
+    if(!capture.isOpened())
+    {
+        cout<<"can't open camera\n";
+        return -1;
+    }
+
+    string leftPath(argv[1]);
+    string rightPath(argv[2]);
+
+    int index = 0;
+
+    while(capture.isOpened())
+    {
+        Mat frame,frameRigth,frameLeft;
+        capture>>frame;
+        frameLeft = frame(cv::Rect(0,0,640,480));
+        frameRigth = frame(cv::Rect(640,0,640,480));
+        imshow("frameLeft",frameLeft);
+        imshow("frameRight",frameRigth);
+        int key = waitKey(33);
+        if(key == 'q')
+            break;
+        if(key == 'c')
+        {
+            index++;
+            imwrite(leftPath+"/"+to_string(index)+".jpg",frameLeft);
+            imwrite(rightPath+"/"+to_string(index)+".jpg",frameRigth);
+        }
+    }
+    return 0;
+}
